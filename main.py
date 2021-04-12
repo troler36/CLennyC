b5d7e525e89acee177fe952da3e8a7f29294aeb6
troler36 <39880578+troler36@users.noreply.github.com>
2021-04-12 18:39:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+Ohit0s = input("Type out any alternative of 0 you want: ")
+Isayv = input("Type out any alternative of v you want: ")
+print(f"This is your Lenny creation: ({Ohit0s}{Isayv}{Ohit0s})")
+print(f"more-> [{Ohit0s}{Isayv}{Ohit0s}] |{Ohit0s}{Isayv}{Ohit0s}| ¯\_({Ohit0s}{Isayv}{Ohit0s})_/¯ ┐({Ohit0s}{Isayv}{Ohit0s})┌ ლ({Ohit0s}{Isayv}{Ohit0s}ლ)")
